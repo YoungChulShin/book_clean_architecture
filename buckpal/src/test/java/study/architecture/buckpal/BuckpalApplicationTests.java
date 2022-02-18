d6c718c6e8af8f5f08f0dc4103f6be871f51a6ff
YoungChulShin <go1323@gmail.com>
2022-02-19 01:35:14 +0900

    초기 소스

---
+++ buckpal/src/test/java/study/architecture/buckpal/BuckpalApplicationTests.java
@@ -0,0 +1,13 @@
+package study.architecture.buckpal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuckpalApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
